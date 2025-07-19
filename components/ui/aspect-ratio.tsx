08be527af5f9e672227bff8e1372ec35036120ca
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2025-07-20 01:41:51 +0900

    Fix Radix UI version-specific imports and add missing dependencies

--- components/ui/aspect-ratio.tsx
+++ components/ui/aspect-ratio.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio@1.1.2";
+import * as React from "react";
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+
+import { cn } from "./utils";
 
 function AspectRatio({
   ...props
